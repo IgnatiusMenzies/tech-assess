dfbf86ee2920b395996abc5001ee03d14fa9e252
Ignatius Menzies <ignatius.menzies@gmail.com>
2017-06-29 06:06:38 +1200

    refining pdf scrape

--- src/download-data.r
+++ src/download-data.r
@@ -22,8 +22,23 @@ tab1 <- future[[57]]
 writeLines(tab1, "tab1.txt")
 
 names_tab1 <- c("rank", "probability", "label", "SOC_code", "occupation")
-widths <- c(7, 13, 6, 9, NA)
+widths_tab1 <- c(7, 13, 6, 9, NA)
 rows_tab1 <- 39
+skip_rows_tab1 <- 8
+
+tab2 <- future[[58]]
+tab2 <- gsub("Edu-\r\n                                   cation", "Education", tab2)
+writeLines(tab2, "tab2.txt")
+widths_tab2 <- c(6, 14, 6, 9, NA)
+skip_rows_tab2 <- 2
+rows_tab1 <- 48
+
+tab3 <- future[[59]]
+tab3 <- gsub("In-\r\n                                   spectors", "Inspectors", tab3)
+writeLines(tab3, "tab3.txt")
+widths_tab2 <- c(6, 14, 6, 9, NA)
+skip_rows_tab2 <- 2
+rows_tab1 <- 49
 
 read_fwf(
   file = "tab1.txt",
