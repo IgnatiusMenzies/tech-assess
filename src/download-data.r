eb17d0b103d79f74f818973945dc450a5c864445
Ignatius Menzies <ignatius.menzies@gmail.com>
2017-06-29 12:59:39 +1200

    functioning scraping function!!

--- src/download-data.r
+++ src/download-data.r
@@ -17,42 +17,30 @@ download.file(pdfUrl, destfile = "input/future-employment.pdf", mode = "wb")
 }
 
 future <- pdf_text("input/future-employment.pdf")
+page_nos <- c(57:72)
+datalist <- list()
 
 #data format is 'ragged fwf' fixed with with a variable-with last column
-tab1 <- future[[57]]
-writeLines(tab1, "tab1.txt")
+for (i  in page_nos) {
+    dat <- future[[i]]
+    # clean long lines with hyphen
+    dat <- gsub("-\r\n +", "", dat)
+    # clean long lines with no hyphen
+    dat <- gsub("\r\n +", " ", dat)
+    writeLines(dat, sprintf("%s.txt", metadata[page == i, table]))
+    dat <- read_fwf(
+        file = sprintf("%s.txt", metadata[page == i, table]),
+        skip = metadata[page == i, skip_rows],
+        fwf_widths(c(metadata[page == i, col1],
+                     metadata[page == i, col2],
+                     metadata[page == i, col3],
+                     metadata[page == i, col4],
+                     metadata[page == i, col5])),
+        n_max = metadata[page == i, num_rows])
+    dat$i <- i
+    datalist[[i]] <- dat
+}
 
-names_tab1 <- c("rank", "probability", "label", "SOC_code", "occupation")
-widths_tab1 <- c(7, 13, 6, 9, NA)
-rows_tab1 <- 39
-skip_rows_tab1 <- 8
-
-tab2 <- future[[58]]
-tab2 <- gsub("Edu-\r\n                                   cation", "Education", tab2)
-writeLines(tab2, "tab2.txt")
-widths_tab2 <- c(6, 14, 6, 9, NA)
-skip_rows_tab2 <- 2
-rows_tab1 <- 48
-
-tab3 <- future[[59]]
-tab3 <- gsub("In-\r\n                                   spectors", "Inspectors", tab3)
-writeLines(tab3, "tab3.txt")
-widths_tab3 <- c(6, 14, 6, 9, NA)
-skip_rows_tab2 <- 2
-rows_tab1 <- 47
-
-tab4 <- future[[60]]
-writeLines(tab4, "tab4.txt")
-
-tab4 <- future[[60]]
-#hyphen
-tab3 <- gsub("-\r\n +", "", tab3)
-#no hyphen
-tab3 <- gsub("\r\n +", " ", tab3)
-writeLines(tab4, "tab4.txt")
-table3 <- read_fwf(
-  file = "tab3.txt",
-  skip = 2,
-  fwf_widths(widths_tab3),
-  colnames(names_tab1),
-  n_max = rows_tab1)
+df <- data.table::rbindlist(datalist)
+names(df) <- c("rank", "probability", "label", "SOC_code", "occupation", "page")
+#correlation between SOC code and census data?
