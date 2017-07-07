92dae21ff867bafe454c2c9c82bfad45436c118c
Ignatius Menzies <ignatius.menzies@gmail.com>
2017-07-07 20:17:23 +1200

    polished download and read functions

--- src/download-data.r
+++ src/download-data.r
@@ -1,4 +1,6 @@
-# Download and read files
+#############################
+## Download and read files ##
+#############################
 
 library(lubridate)
 library(pdftools)
@@ -12,19 +14,12 @@ metadata_download <- fread("input/metadata/metadata_download.csv")
 metadata_pdf <- fread("input/metadata/metadata_pdf.csv")
 metadata_xls <- fread("input/metadata/metadata_xls.csv")
 
-rdata_files <- c("anzsco_isco.rdata", "future_jobs.rdata", "isco_soc.rdata")
-
 # download files if they don't exist
-if (!file.exists("input/raw/*.xls | input/raw/*.pdf")) {
-    download_files(metadata_download)
-}
+download_files(metadata_download)
 
 # read in data if they haven't been
-if (!(grep(".*\\.rdata$",list.files("input/")))) {
-    read_files(metadata_download, metadata_pdf, metadata_xls)
-}
+read_files(metadata_download, metadata_pdf, metadata_xls)
 
-list.files("input/")[grepl(".*\\.rdata$",list.files("input/"))]
-
-# page numbers need to be removed
+## load and join tables
+#list.files("input/")[grepl(".*\\.rdata$",list.files("input/"))]
 #correlation between SOC code and census data?
--- src/helpers.r
+++ src/helpers.r
@@ -15,7 +15,7 @@ download_files <- function(x) {
             download.file(x[i,url],
                           destfile = sprintf("input/raw/%s", x[i,file]),
                           mode = "wb")
-        } else cat("\ninput\raw", x[i,file], " already downloaded \n")
+        } else cat(x[i,file], "already downloaded \n")
     }
 }
 
@@ -37,53 +37,61 @@ read_files <- function(download_meta, pdf_meta, xls_meta){
 }
 
 # takes (x) a data.table of metadata for excel files with a particular schema
-read_xls_files <- function(x){
-    df <- read_excel(
-        path = sprintf("input/raw/%s", x[,file]),
-        sheet = x[,sheet],
-        range = x[,range],
-        col_names = c(x[,head1],
-                      x[,head2],
-                      x[,head3],
-                      x[,head4],
-                      x[,head5])
-    )
-    save(df, file = sprintf("input/%s", gsub("\\.xlsx?$", "\\.rdata", x[,file])))
+read_xls_files <- function(x) {
+    cat("\n\n===== Reading file: ", x[,file], "\n")
+    if (!file.exists(sprintf("input/%s", gsub("\\.xlsx?$", "\\.rdata", x[,file])))) {
+        df <- read_excel(
+            path = sprintf("input/raw/%s", x[,file]),
+            sheet = x[,sheet],
+            range = x[,range],
+            col_names = c(x[,head1],
+                          x[,head2],
+                          x[,head3],
+                          x[,head4],
+                          x[,head5])
+            )
+        save(df, file = sprintf("input/%s", gsub("\\.xlsx?$", "\\.rdata", x[,file])))
+    } else cat(gsub("\\.xlsx?$", "\\.rdata", x[,file]), "already exists \n")
 }
-
+    
 read_pdf_files <- function(x) {
-    future <- pdf_text(sprintf("input/raw/%s", x[1,file]))
-    page_nos <- as.numeric(min(x[,page]):max(x[,page]))
-    datalist <- list()
-    #data format is 'ragged fwf' fixed with with a variable-with last column
-    for (i  in page_nos) {
-        dat <- future[[i]]
-        if (i != 57) {
-            # clean long lines with hyphen
-            dat <- gsub("-\n +", "", dat)
-            # clean long lines with no hyphen
-            dat <- gsub(" {0}\n +", " ", dat)
+    cat("\n\n===== Reading file: ", x[1,file], "\n")
+    if (!file.exists(sprintf("input/%s", gsub("\\.pdf?$", "\\.rdata", x[1,file])))) {
+        future <- pdf_text(sprintf("input/raw/%s", x[1,file]))
+        page_nos <- as.numeric(min(x[,page]):max(x[,page]))
+        datalist <- list()
+        #data format is 'ragged fwf' fixed with with a variable-with last column
+        for (i  in page_nos) {
+            dat <- future[[i]]
+                if (i != 57) {
+                # clean long lines with hyphen
+                dat <- gsub("-\n +", "", dat)
+                # clean long lines with no hyphen
+                dat <- gsub(" {0}\n +", " ", dat)
+                }
+            writeLines(dat, sprintf("%s.txt", x[page == i, table]))
+            dat <- read_fwf(
+                file = sprintf("%s.txt", x[page == i, table]),
+                skip = x[page == i, skip_rows],
+                fwf_widths(widths = c(x[page == i, col1],
+                                      x[page == i, col2],
+                                      x[page == i, col3],
+                                      x[page == i, col4],
+                                      x[page == i, col5]), 
+                            col_names = c("rank", 
+                                         "probability", 
+                                         "label", 
+                                         "SOC_code", 
+                                        "occupation")),
+                n_max = as.numeric(x[page == i, num_rows]))
+            dat$page <- i
+            datalist[[i]] <- dat
+            unlink(sprintf("%s.txt", x[page == i, table]))
+            dat <- NULL
         }
-        writeLines(dat, sprintf("%s.txt", x[page == i, table]))
-        dat <- read_fwf(
-            file = sprintf("%s.txt", x[page == i, table]),
-            skip = x[page == i, skip_rows],
-            fwf_widths(widths = c(x[page == i, col1],
-                                  x[page == i, col2],
-                                  x[page == i, col3],
-                                  x[page == i, col4],
-                                  x[page == i, col5]), 
-                       col_names = c("rank", 
-                                     "probability", 
-                                     "label", 
-                                     "SOC_code", 
-                                     "occupation")),
-            n_max = as.numeric(x[page == i, num_rows]))
-        dat$page <- i
-        datalist[[i]] <- dat
-        unlink(sprintf("%s.txt", x[page == i, table]))
-        dat <- NULL
-    }
-    df <- data.table::rbindlist(datalist)
-    save(df, file = "input/future_jobs.rdata")
+        df <- data.table::rbindlist(datalist)
+        # remove page numbers
+        df[, occupation := gsub(" \\d{2}", "", occupation)]
+        save(df, file = "input/future_employment.rdata")
+    } else cat(gsub("\\.pdf?$", "\\.rdata", x[1,file]), "already exists \n")
 }
