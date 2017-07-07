99fc6c7812fe26c6a78ca55bb3934c9d91fe1d21
Ignatius Menzies <ignatius.menzies@gmail.com>
2017-07-07 19:15:36 +1200

    download and read functions functioning

--- src/download-data.r
+++ src/download-data.r
@@ -1,68 +1,30 @@
-##############################
-##Download and extract data ##
-##############################
+# Download and read files
 
 library(lubridate)
 library(pdftools)
 library(readr)
 library(data.table)
 library(readxl)
-metadata_download <- fread("input/metadata_download.csv")
-metadata_pdf <- fread("input/metadata_pdf.csv")
-metadata_xls <- fread("input/metadata_xls.csv")
+source("src/helpers.r")
 
-####################
-## download files ##
-####################
+#read metadata files 
+metadata_download <- fread("input/metadata/metadata_download.csv")
+metadata_pdf <- fread("input/metadata/metadata_pdf.csv")
+metadata_xls <- fread("input/metadata/metadata_xls.csv")
 
-# requires a metadata file with 2 columns: 'file' and 'url'
-download_files <- function(x) {
-    for (i in 1:nrow(x)) { 
-        cat("\n\n===== Processing file: ", x[i,file], "\n")
-        if (!file.exists(sprintf("input/%s", x[i,file]))) {
-            cat("\n Downloading ", x[i,file])
-            download.file(x[i,url],
-                  destfile = sprintf("input/%s", x[i,file]),
-                  mode = "wb")
-        } else cat("\ninput", x[i,file], " already downloaded \n")
-    }
+rdata_files <- c("anzsco_isco.rdata", "future_jobs.rdata", "isco_soc.rdata")
+
+# download files if they don't exist
+if (!file.exists("input/raw/*.xls | input/raw/*.pdf")) {
+    download_files(metadata_download)
 }
 
-download_files(metadata_download)
-
-if (!file.exists("input/future_jobs.rdata")) {
-
-future <- pdf_text("input/future-employment.pdf")
-page_nos <- as.numeric(57:72)
-datalist <- list()
-
-#data format is 'ragged fwf' fixed with with a variable-with last column
-for (i  in page_nos) {
-    dat <- future[[i]]
-    if (i != 57) {
-    # clean long lines with hyphen
-    dat <- gsub("-\n +", "", dat)
-    # clean long lines with no hyphen
-    dat <- gsub(" {0}\n +", " ", dat)
-    }
-    writeLines(dat, sprintf("%s.txt", metadata[page == i, table]))
-    dat <- read_fwf(
-        file = sprintf("%s.txt", metadata[page == i, table]),
-        skip = metadata[page == i, skip_rows],
-        fwf_widths(widths = c(metadata[page == i, col1],
-                     metadata[page == i, col2],
-                     metadata[page == i, col3],
-                     metadata[page == i, col4],
-                     metadata[page == i, col5]), col_names = c("rank", "probability", "label", "SOC_code", "occupation")),
-        n_max = as.numeric(metadata[page == i, num_rows]))
-    dat$page <- i
-    datalist[[i]] <- dat
-    unlink(sprintf("%s.txt", metadata[page == i, table]))
-    dat <- NULL
+# read in data if they haven't been
+if (!(grep(".*\\.rdata$",list.files("input/")))) {
+    read_files(metadata_download, metadata_pdf, metadata_xls)
 }
 
-df <- data.table::rbindlist(datalist)
-save(df, file = "input/future_jobs.rdata")
-}
+list.files("input/")[grepl(".*\\.rdata$",list.files("input/"))]
+
 # page numbers need to be removed
 #correlation between SOC code and census data?
--- src/helpers.r
+++ src/helpers.r
@@ -1,20 +1,89 @@
-######################
-## Helper functions ##
-######################
+#########################################
+## Functions to download and read data ##
+#########################################
 
-# function for grooming pdf table data
-groom.tables <- function(table) {
-    if (table == "tab2") {
-        tab2 <- gsub("Edu-\r\n                                   cation", "Education", tab2)
-    }
-    if (table == "tab3") {
-        tab3 <- gsub("In-\r\n                                   spectors", "Inspectors", tab3)
-        tab3 <- gsub("Machine\r\n                                   and", "Machine and", tab3)
-    }
-    if (table == "tab5") {
-        tab5 <- gsub("-\r\n                                   ", "", tab5)
-        tab5 <- gsub("In-\r\n                                   structors", "Instructors", tab5)
+####################
+## download files ##
+####################
+
+# requires a metadata file with 2 columns: 'file' and 'url'
+download_files <- function(x) {
+    for (i in 1:nrow(x)) { 
+        cat("\n\n===== Processing file: ", x[i,file], "\n")
+        if (!file.exists(sprintf("input/raw/%s", x[i,file]))) {
+            cat("\n Downloading ", x[i,file])
+            download.file(x[i,url],
+                          destfile = sprintf("input/raw/%s", x[i,file]),
+                          mode = "wb")
+        } else cat("\ninput\raw", x[i,file], " already downloaded \n")
     }
 }
 
-# no changes for 1,4
+################
+## read files ##
+################
+
+# requires download, pdf and xls metadata files
+read_files <- function(download_meta, pdf_meta, xls_meta){
+    for (i in 1:nrow(download_meta)) {
+        if (download_meta[i,file_type == "excel"]) {
+            read_xls_files(xls_meta[file == download_meta[i,file],])
+        }
+        if (download_meta[i,file_type == "pdf"]) {
+            
+            read_pdf_files(pdf_meta[file == download_meta[i,file],])
+        }
+    }
+}
+
+# takes (x) a data.table of metadata for excel files with a particular schema
+read_xls_files <- function(x){
+    df <- read_excel(
+        path = sprintf("input/raw/%s", x[,file]),
+        sheet = x[,sheet],
+        range = x[,range],
+        col_names = c(x[,head1],
+                      x[,head2],
+                      x[,head3],
+                      x[,head4],
+                      x[,head5])
+    )
+    save(df, file = sprintf("input/%s", gsub("\\.xlsx?$", "\\.rdata", x[,file])))
+}
+
+read_pdf_files <- function(x) {
+    future <- pdf_text(sprintf("input/raw/%s", x[1,file]))
+    page_nos <- as.numeric(min(x[,page]):max(x[,page]))
+    datalist <- list()
+    #data format is 'ragged fwf' fixed with with a variable-with last column
+    for (i  in page_nos) {
+        dat <- future[[i]]
+        if (i != 57) {
+            # clean long lines with hyphen
+            dat <- gsub("-\n +", "", dat)
+            # clean long lines with no hyphen
+            dat <- gsub(" {0}\n +", " ", dat)
+        }
+        writeLines(dat, sprintf("%s.txt", x[page == i, table]))
+        dat <- read_fwf(
+            file = sprintf("%s.txt", x[page == i, table]),
+            skip = x[page == i, skip_rows],
+            fwf_widths(widths = c(x[page == i, col1],
+                                  x[page == i, col2],
+                                  x[page == i, col3],
+                                  x[page == i, col4],
+                                  x[page == i, col5]), 
+                       col_names = c("rank", 
+                                     "probability", 
+                                     "label", 
+                                     "SOC_code", 
+                                     "occupation")),
+            n_max = as.numeric(x[page == i, num_rows]))
+        dat$page <- i
+        datalist[[i]] <- dat
+        unlink(sprintf("%s.txt", x[page == i, table]))
+        dat <- NULL
+    }
+    df <- data.table::rbindlist(datalist)
+    save(df, file = "input/future_jobs.rdata")
+}
